1e03b2f8eb11a001221ea8c0604dc65ea46c0022
AlbinDeploy <albinpremam12@gmail.com>
2026-06-14 06:33:23 -0700

    Upload besar-besaran

---
+++ db/server_data.php
@@ -0,0 +1,12 @@
+server|139.99.222.156
+port|55000
+type|2
+loginurl|login.albintsx.site
+#maint|Server currently change hosting, please join discord.gg/mcps to get the latest host.
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
